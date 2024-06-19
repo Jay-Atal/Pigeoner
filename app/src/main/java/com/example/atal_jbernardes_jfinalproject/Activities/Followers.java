f9683e4776ae7f7cd4da45b3f3d583a99a84b03d
Joel Bernardes <119889298+JoelBernardes@users.noreply.github.com>
2024-06-19 18:50:11 -0400

    Added activites and fragments needed with some UI

--- app/src/main/java/com/example/atal_jbernardes_jfinalproject/Activities/Followers.java
+++ app/src/main/java/com/example/atal_jbernardes_jfinalproject/Activities/Followers.java
@@ -1,4 +1,4 @@
-package com.example.atal_jbernardes_jfinalproject;
+package com.example.atal_jbernardes_jfinalproject.Activities;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.atal_jbernardes_jfinalproject.R;
 
 public class Followers extends AppCompatActivity {
 
